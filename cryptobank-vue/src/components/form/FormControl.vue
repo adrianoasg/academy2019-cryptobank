554d954511bd8f32e24e3a6abe3d494e1dfadc63
adriano <adrianoasg@hotmail.com>
2019-07-29 23:22:52 -0300

    adiciona componente formControl

---
+++ cryptobank-vue/src/components/form/FormControl.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="form-control">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FormControl'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.form-control {
+  display: block;
+  margin-top: 2px;
+  margin-bottom: 2px;
+}
+
+.form-control > input:focus {
+  outline: 2px solid #354463;
+}
+
+
+</style>
