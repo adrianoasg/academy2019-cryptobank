f8eb78f75f9cf5049da1d31b149d33d4285329fa
adriano <adrianoasg@hotmail.com>
2019-07-29 23:35:35 -0300

    adiciona componente button

---
+++ cryptobank-vue/src/components/form/Button.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="actions">
+    <button type="submit" class="button">
+      {{ labelButton }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    labelButton: {
+      require: true
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.actions {
+  padding: 15px 15px;
+
+}
+.button {
+  display: block;
+
+  background-color: #FA7268;
+  border: 0;
+  border-radius: 5px;
+  color: #FFF;
+  padding: 10px 30px;
+  font-family: 'Roboto', sans-serif;
+  font-weight: bold;
+  font-size: 20px;
+  width: 350px;
+  height: 50px;
+  cursor: pointer;
+
+  margin: 0 auto;
+}
+</style>
